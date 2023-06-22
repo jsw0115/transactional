6eac66faac244e567e766723ac16cb9aba2acd6f
jsw0115 <jsw0101151@naver.com>
2023-06-22 17:43:39 +0900

    transactional 세미나 준비 프로젝트

--- src/main/java/kr/jpa/transactional/jpa/dto/BoardDTO.java
+++ src/main/java/kr/jpa/transactional/jpa/dto/BoardDTO.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor  // 기본 생성자
 @AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
 public class BoardDTO {
+
     private Long id;
     private String boardWriter;
     private String boardPass;
--- src/main/java/kr/jpa/transactional/jpa/entity/BoardEntity.java
+++ src/main/java/kr/jpa/transactional/jpa/entity/BoardEntity.java
@@ -1,10 +1,10 @@
 package kr.jpa.transactional.jpa.entity;
 
+import kr.jpa.transactional.jpa.dto.BoardDTO;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 // DB 테이블 역할 하는 클래스
 @Entity
@@ -13,4 +13,43 @@ import javax.persistence.Table;
 @Table(name = "board_table")
 public class BoardEntity extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String boardWriter;
+
+    @Column
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity (BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0);
+        return boardEntity;
+    }
+
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(boardEntity.getBoardHits());
+        return boardEntity;
+    }
 }
